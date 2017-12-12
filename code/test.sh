c43a28d1c4771791271476a137a8fbe741f072fe
Vishal Lal <vish61@gmail.com>
2017-12-12 13:22:55 +0000

    Revert port change

--- code/test.sh
+++ code/test.sh
@@ -6,7 +6,7 @@ ID=$! # ID of webserver process, so we can kill it
 
 tests_passed=true
 expected="Hello From Golang"
-output=$(curl -s localhost:8081)
+output=$(curl -s localhost:8080)
 if [[ "$expected" != "$output" ]]; then
   echo "Test Failure"
   echo "$expected != $output"
